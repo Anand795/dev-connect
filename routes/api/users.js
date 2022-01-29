bf2f512169d04ceadcdc89b98a6adecbe21ee78f
Anand <anands7957@gmail.com>
2022-01-29 14:39:19 +0530

    post call and validations for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator"); // froentend validations like isEmila, password length
 
-// @route       GET api/users
-// @desc        Test Route
+// @route       POST api/users
+// @desc        Register User
 // @access      Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    // First Parm is property and 2nd is error message
+    check("name", "Name is Required"  ).not().isEmpty(),
+    check("email", "Please Include valid Email").isEmail(),
+    check(
+      "password",
+      "please enter password with 6 or more charcters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // in order to work req.body -> we have to initialize the middlewhare for the body parser
+    // I have done it in the server.js file   
+    console.log(req.body);
+    // Set Error response
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
 module.exports = router;
