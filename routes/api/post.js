b1dbcc33b302a775db3074950249eba89c87f912
Anand <anands7957@gmail.com>
2022-02-19 21:00:43 +0530

    post like route

--- routes/api/post.js
+++ routes/api/post.js
@@ -73,7 +73,7 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
-// @route       DELETE api/posts/:od
+// @route       DELETE api/posts/:id
 // @desc        Delete a post
 // @access      Private
 router.delete("/:id", auth, async (req, res) => {
@@ -95,6 +95,30 @@ router.delete("/:id", auth, async (req, res) => {
     if (err.kind === "ObjectId") {
       return res.status(404).json({ msg: "No posts found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route       PUT api/posts/like/:id
+// @desc        Like a post
+// @access      Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //   check if the user already liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
