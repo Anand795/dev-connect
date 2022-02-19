3e1068f7ae778ad4caca0f98d87c53f054b0fa32
Anand <anands7957@gmail.com>
2022-02-19 19:02:45 +0530

    get all posts

--- routes/api/post.js
+++ routes/api/post.js
@@ -38,4 +38,20 @@ router.post(
   }
 );
 
+// @route       GET api/posts
+// @desc        Get all post
+// @access      Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    if (!posts) {
+      return res.status(400).json({ msg: "No posts found" });
+    }
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
