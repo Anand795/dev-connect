67f0ee50e3d23f00d7a15b9fdaaebcc4951196de
Anand <anands7957@gmail.com>
2022-03-07 09:08:05 +0530

    profile display

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,9 @@ import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import ProfileForm from "./components/profile-form/ProfileForm";
-import AddExperience from "./components/profile-form/AddExperience";  
+import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 import "./App.css";
 
 // Redux
@@ -38,8 +39,9 @@ const App = () => {
         <Alert />
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profiles" element={<Profiles />} />
           <Route
             path="dashboard"
             element={<PrivateRoute component={Dashboard} />}
