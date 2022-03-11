1c0d27bb4d209b4ac86cf1496010f8edf59e23b1
Anand <anands7957@gmail.com>
2022-03-11 13:38:00 +0530

    like and unlike post functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import api from "../utils/api";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get Post
 export const getPosts = (id) => async (dispatch) => {
@@ -16,6 +16,40 @@ export const getPosts = (id) => async (dispatch) => {
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add like
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await api.put(`/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Remove like
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await api.put(`/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
     });
   }
 };
--- client/src/components/posts/PostItem.jsx
+++ client/src/components/posts/PostItem.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
-import formatDate from '../../utils/formatDate';
-import { connect } from 'react-redux';
+import React ,{Fragment} from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import formatDate from "../../utils/formatDate";
+import { connect } from "react-redux";
+import { addLike, removeLike } from "../../actions/post";
 
 const PostItem = ({
-    auth,
-    post: { _id, text, name, avatar, user, likes, comments, date },
-    showActions
-  }) => (
-    <div className="post bg-white p-1 my-1">
-      <div>
-        <Link to={`/profile/${user}`}>
-          <img className="round-img" src={avatar} alt="" />
-          <h4>{name}</h4>
-        </Link>
-      </div>
-      <div>
-        <p className="my-1">{text}</p>
-        <p className="post-date">Posted on {formatDate(date)}</p>
-  
-        {/* {showActions && (
+  addLike,
+  removeLike,
+  auth,
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  showActions,
+}) => (
+  <div className="post bg-white p-1 my-1">
+    <div>
+      <Link to={`/profile/${user}`}>
+        <img className="round-img" src={avatar} alt="" />
+        <h4>{name}</h4>
+      </Link>
+    </div>
+    <div>
+      <p className="my-1">{text}</p>
+      <p className="post-date">Posted on {formatDate(date)}</p>
+
+      {true && (
           <Fragment>
             <button
               onClick={() => addLike(_id)}
@@ -37,7 +40,7 @@ const PostItem = ({
             >
               <i className="fas fa-thumbs-down" />
             </button>
-            <Link to={`/posts/${_id}`} className="btn btn-primary">
+            {/* <Link to={`/posts/${_id}`} className="btn btn-primary">
               Discussion{' '}
               {comments.length > 0 && (
                 <span className="comment-count">{comments.length}</span>
@@ -51,26 +54,24 @@ const PostItem = ({
               >
                 <i className="fas fa-times" />
               </button>
-            )}
+            )} */}
           </Fragment>
-        )} */}
-      </div>
+        )}
     </div>
-  );
+  </div>
+);
 
 PostItem.propTypes = {
-    post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired,
-    // addLike: PropTypes.func.isRequired,
-    // removeLike: PropTypes.func.isRequired,
-    // deletePost: PropTypes.func.isRequired,
-    // showActions: PropTypes.bool
-}
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  // deletePost: PropTypes.func.isRequired,
+  // showActions: PropTypes.bool
+};
 
 const mapStateToProps = (state) => ({
-    auth: state.authReducer
-  });
+  auth: state.authReducer,
+});
 
-export default connect(mapStateToProps, { })(
-    PostItem
-  );
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
